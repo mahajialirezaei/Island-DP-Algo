390c4e0cd02a9a763316cd4d88bf6759559bbc1c
mahajialirezaei <m.a.hajialirezaei05@gmail.com>
2025-04-27 15:48:18 +0330

    fully implemented

--- Island.py
+++ Island.py
@@ -23,15 +23,20 @@ def probability_death(x: int, y: int, m: int, n: int):
             dp[i].append([])
             for k in range(m + 1):
                 dp[i][j].append(0)
+
+    for i in range(n):
+        for j in range(n):
+            if i in ls or j in ls:
+                dp[i][j][1] = 0.25
+            if i in ls and j in ls:
+                dp[i][j][1] = 0.5
+
     for k in range(1, m + 1):
         for i in range(n):
             for j in range(n):
-                if i in ls or j in ls:
-                    dp[i][j][1] = 0.25
-                if i in ls and j in ls:
-                    dp[i][j][1] = 0.5
-                dp[i][j][0] = 0
-
+                if (i in ls or j in ls) and k==1:
+                    continue
+                print(i, j, k, dp[i][j][k])
                 if i < n - 1:
                     dp[i][j][k] += dp[i + 1][j][k - 1] / 4
                 else:
@@ -51,9 +56,8 @@ def probability_death(x: int, y: int, m: int, n: int):
                     dp[i][j][k] += dp[i][j - 1][k - 1] / 4
                 else:
                     dp[i][j][k] += 0.25
+                print(i, j, k, dp[i][j][k])
 
-
-            dp[i][j][0] = 0
 
     for k in range(m + 1):
         print_table(dp, k)
@@ -66,4 +70,4 @@ x = 1
 y = 1
 m = 3
 # print(probability_death(x, y, m, n))
-print(probability_death(1, 1, 2, 3))
+print(probability_death(1, 1, 4, 5))
