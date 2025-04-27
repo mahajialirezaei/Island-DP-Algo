04966a34c40274832c53aaa23ded1963b49c5902
mahajialirezaei <m.a.hajialirezaei05@gmail.com>
2025-04-27 14:12:53 +0330

    edit for

--- Island.py
+++ Island.py
@@ -1,33 +1,61 @@
-def probability_death(x:int, y:int, m:int, n:int):
+def print_table(dp, k):
+    for i in range(len(dp)):
+        for j in range(len(dp[i])):
+            print(dp[i][j][k], end=" ")
+        print()
+    print("\n")
+
+def probability_death(x: int, y: int, m: int, n: int):
     dp = []
-    ls = [0, n-1]
-    for i in range(n+1):
-        dp.append([])
-        for j in range(n+1):
-            dp[i].append([])
-            for k in range(m+1):
-                dp[i][j].append(0)
-
+    ls = [0, n - 1]
     for i in range(n):
+        dp.append([])
         for j in range(n):
-            for k in range(m+1):
-                if (i in ls and j in ls):
+            dp[i].append([])
+            for k in range(m + 1):
+                dp[i][j].append(0)
+    for k in range(1, m + 1):
+        for i in range(n):
+            for j in range(n):
+                if i in ls or j in ls:
                     dp[i][j][1] = 0.25
-                if (i in ls and i == j):
+                if i in ls and j in ls:
                     dp[i][j][1] = 0.5
-                if k == 0:
-                    dp[i][j][k] = 0
-                dp[i][j][k] = (dp[i-1][j][k-1] + dp[i][j-1][k -1] + dp[i+1][j][k-1] + dp[i][j+1][k-1]) / 4
-                print(dp[i][j][k], i, j, k)
+                dp[i][j][0] = 0
+                count = 0
+                if i < n - 1:
+                    dp[i][j][k] += dp[i + 1][j][k - 1]
+                    count += 1
+
+                if j < n - 1:
+                    dp[i][j][k] += dp[i][j + 1][k - 1]
+                    count += 1
+
+                if i > 0:
+                    dp[i][j][k] += dp[i - 1][j][k - 1]
+                    count += 1
+
+                if j > 0:
+                    dp[i][j][k] += dp[i][j - 1][k - 1]
+                    count += 1
+
+
+                if count > 0 and i not in ls and j not in ls:
+                    dp[i][j][k] /= count
+            dp[i][j][0] = 0
+
+    for k in range(m + 1):
+        print_table(dp, k)
+
     return dp[x][y][m]
-
-
 
 n = 5
 x = 1
-y= 1
+y = 1
 m = 3
 print(probability_death(x, y, m, n))
+# print(probability_death(1, 1, 2, 3))
+
 
 
 
